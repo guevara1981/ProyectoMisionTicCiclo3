f02654736067c505b65e4de1a04abbc4d2178a41
Cristhian Medina <cristhian.medina.or@hotmail.com>
2022-09-18 20:38:11 -0500

    Proyecto

---
+++ agenda-lab/src/main/java/proyectov1/agendalab/AgendaLabApplication.java
@@ -0,0 +1,13 @@
+package proyectov1.agendalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaLabApplication.class, args);
+	}
+
+}
---
+++ agenda-lab/src/test/java/proyectov1/agendalab/AgendaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectov1.agendalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
