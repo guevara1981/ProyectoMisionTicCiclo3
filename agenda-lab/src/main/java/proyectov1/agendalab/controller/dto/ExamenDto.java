c5f7c65ba5153e9faa07447afae2e6da88bce11e
Cristhian Medina <cristhian.medina.or@hotmail.com>
2022-10-02 13:45:14 -0500

    Listar Examenes

--- agenda-lab/src/main/java/proyectov1/agendalab/controller/dto/ExamenDto.java
+++ agenda-lab/src/main/java/proyectov1/agendalab/controller/dto/ExamenDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import proyectov1.agendalab.model.entity.Seccion;
+//import proyectov1.agendalab.model.entity.Seccion;
 
 @Builder
 @Data
